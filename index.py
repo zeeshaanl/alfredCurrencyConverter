fdffadf4013204e36e50f1cc831dce20d5b4c7aa
Zeeshaan Lakdawala <external.zeeshaan.lakdawala@kuehne-nagel.com>
2020-02-08 18:47:11 +0100

    write script

---
+++ index.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+
+# https://exchangeratesapi.io/
+
+import http.client
+import json
+import sys
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+args = sys.argv[1]
+
+if len(args.split(" ")) > 3:
+    splitInput = args.split(" ")
+    amount = float(splitInput[0])
+    fromC = splitInput[1].upper()
+    toC = splitInput[3].upper()
+
+    conn = http.client.HTTPSConnection("api.exchangeratesapi.io")
+
+    conn.request("GET", f'/latest?base={fromC}&symbols={toC}')
+
+    r1 = conn.getresponse()
+    data1 = r1.read()
+    jsonData = json.loads(data1)
+
+    oneOfTarget = jsonData["rates"][toC]
+
+    convertedAmount = oneOfTarget*amount
+
+    result = {
+            "items": [
+                {
+                    "title": convertedAmount,
+                    "subtitle": toC,
+                    "arg": convertedAmount,
+                    "icon": {
+                        "path": "cicon.png"
+                    }
+                }
+            ]
+        }
+    alfredOutput = json.dumps(result)
+
+    print(alfredOutput)
+
+else:
+    result = {
+            "items": [
+                {
+                    "title": "Waiting to finish request...",
+                    "icon": {
+                        "path": "cicon.png"
+                    }
+                }
+            ]
+        }
+    alfredOutput = json.dumps(result)
+
+    print(alfredOutput)
